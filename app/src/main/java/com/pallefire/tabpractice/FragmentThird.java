a476dd718ee4d357fe2aef5515bda6e1e25a93bc
Mirza Aqib <MirzaAqibBeg@gmail.com>
2017-01-19 19:05:48 +0530

    Tab example

---
+++ app/src/main/java/com/pallefire/tabpractice/FragmentThird.java
@@ -0,0 +1,29 @@
+package com.pallefire.tabpractice;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentThird extends Fragment {
+
+
+    public FragmentThird() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_third, container, false);
+    }
+
+}
